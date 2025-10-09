f886f9f2e4577b363f9260fecfacad86a21e2c69
RonMalu <malucheichengr@gmail.com>
2025-10-09 19:15:34 +1000

    adding migration file for FK c_id to models and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_111840) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_09_083715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,5 +25,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_08_111840) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 end
